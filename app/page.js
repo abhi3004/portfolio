1824ecc826d3d419af70fe5721a1f13574fbfdf7
abhi3004 <abhijeet.pareek30@gmail.com>
2023-11-28 16:43:09 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import React, { useState } from 'react';
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  const [projects, setProjects] = useState([]);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -90,6 +92,13 @@ export default function Home() {
           </p>
         </a>
       </div>
+    {projects.map(project => {
+      return (
+      <div key={project.id}>
+        
+      </div>
+      );
+    })}
     </main>
   )
 }
